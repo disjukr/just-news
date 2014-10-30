9d44cd5839919c9ea4357357a6da9a2fb27b1e97
JongChan Choi <disjukr@naver.com>
2014-10-30 14:31:22 +0900

    0.6.0

--- jews.user.js
+++ jews.user.js
@@ -2,7 +2,7 @@
 // @name jews
 // @namespace http://0xABCDEF.com/jews
 // @description just news
-// @version 0.5.0
+// @version 0.6.0
 // @updateURL https://raw.githubusercontent.com/disjukr/jews/release/jews.user.js
 // @downloadURL https://raw.githubusercontent.com/disjukr/jews/release/jews.user.js
 // @include http://news.jtbc.joins.com/html/*
