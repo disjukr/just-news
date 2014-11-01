c4173734f440d2d431051a3333f9abd9ebad1172
ChaYoung You <yousbe@gmail.com>
2014-11-02 03:00:12 +0900

    Clean up

--- jews.user.js
+++ jews.user.js
@@ -849,19 +849,21 @@ parse['세계일보'] = function (jews) {
     jews.title = document.querySelector('.container>.content>.titleh1>h1').childNodes[0].textContent;
     jews.subtitle = document.querySelector('.container>.content>.titleh2>h2').innerText;
     jews.content = clearStyles(document.getElementById('article_txt')).innerHTML;
-    jews.timestamp = {created: undefined, lastModified: undefined};
+    jews.timestamp = { created: undefined, lastModified: undefined };
     document.getElementById('SG_ArticleDateLine').innerText
         .replace(/(입력|수정)\s*(\d{4}-\d{2}-\d{2})\s*(\d{2}:\d{2}:\d{2})/g, function(_, p1, p2, p3){
             var ts = p2 + 'T' + p3 + '+09:00'; // ISO 8601
             if (p1 === '입력') jews.timestamp.created = new Date(ts);
             else if (p1 === '수정') jews.timestamp.lastModified = new Date(ts);
         });
-    var r = ('\n'+document.getElementById('article_txt').innerText+'\n').match(/\n([^\n@]+)\s기자\s([^가-힣\s\n]+@segye.com)\n/);
+    var r = ('\n' + document.getElementById('article_txt').innerText + '\n').match(/\n([^\n@]+)\s기자\s([^가-힣\s\n]+@segye.com)\n/);
     jews.reporters = [];
-    if (r) jews.reporters.push({
-        name: r[1],
-        mail: r[2]
-    });
+    if (r) {
+        jews.reporters.push({
+            name: r[1],
+            mail: r[2]
+        });
+    }
     jews.pesticide = function () {
         $('#scrollDiv, #realclick_view, script, iframe, .mask_div').remove();
     };
