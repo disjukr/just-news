2338952e0ee6e139f256d5c6308644417e59fb8f
즈눅 <xnuk@users.noreply.github.com>
2015-03-03 11:03:20 +0900

    민중의소리: Fixes #112

--- jews.user.js
+++ jews.user.js
@@ -1009,29 +1009,26 @@ parse['민중의소리'] = function (jews, done) {
         // Don't call done() to show unmodified web page.
         return;
     }
-    jews.title = $('.article-header h1').text();
-    jews.subtitle = $('.article-header p').text() || undefined;
+    jews.title = $('.title-zone h2').text();
+    jews.subtitle = $('.title-zone .subtitle').text() || undefined;
     jews.content = (function () {
         var content = document.createElement('div');
-        $('.article .news_photo, .article .article-text').each(function (i, v) {
+        $('.article-body>p, .article-body>.news_photo').each(function (i, v) {
             content.appendChild(v);
         });
         return clearStyles(content).innerHTML;
     })();
-    jews.timestamp = (function () {
-        var parsedData = $('.article-bottom-meta .time');
-        var lastModified;
-        if (parsedData.length > 1) {
-            lastModified = new Date(parsedData.eq(1).text().replace('최종수정 ', '').replace(' ', 'T') + '+09:00');
+    var amb = $('.article-meta-bottom')[0];
+    [].some.call(amb.getElementsByClassName('date'), function (v) {
+        var a=v.textContent.match(/^\s*[가-힣]+ (\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2})\s*$/);
+        if (a != null) {
+            jews.timestamp = {created: undefined, lastModified: new Date(a[1] + 'T' + a[2] + '+09:00')}; // ISO 8601
+            return true;
         }
-        return {
-            created: new Date(parsedData.eq(0).text().replace('발행시간 ', '').replace(' ', 'T') + '+09:00'),
-            lastModified: lastModified
-        };
-    })();
+    });
     jews.reporters = [{
-        name: $('.article-bottom-meta .writer').text().trim(),
-        mail: $('.article-bottom-meta .wrtier-email').text().trim() || undefined
+        name: amb.getElementsByClassName('writer')[0].textContent.trim(),
+        mail: amb.getElementsByClassName('email')[0].textContent.trim() || undefined
     }];
     // Explicitly call done() although this is not asynchronous.
     done();
