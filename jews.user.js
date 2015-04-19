b31dc30307ef6ed713e154a800fdacfc9a525e7d
ChaYoung You <yousbe@gmail.com>
2015-04-19 17:42:50 +0900

    Fix parsing 뉴데일리 (Fix #127)

--- jews.user.js
+++ jews.user.js
@@ -651,26 +651,28 @@ parse['노컷뉴스'] = function (jews) {
     };
 };
 parse['뉴데일리'] = function (jews) {
-    var a = document.getElementById('ndArtTit'),
-        b = a.querySelectorAll('#ndArtOption>li'),
-        c = b[2].childNodes,
-        i = -1, j;
-    while (j = c[++i]) {
-        if (j.nodeType === 3) break;
-    }
-    j = j.textContent;
-    jews.title = a.getElementsByTagName('h1')[0].textContent;
-    jews.subtitle = a.getElementsByTagName('h2')[0].textContent;
+    var titleData = document.getElementById('titlebox');
+    var articleData = document.getElementById('ndArtOption');
+    jews.title = titleData.getElementsByTagName('h1')[0].textContent;
+    jews.subtitle = (function () {
+        var h3 = titleData.getElementsByTagName('h3')[0].textContent;
+        var h2 = titleData.getElementsByTagName('h2')[0].textContent;
+        if (h3) {
+            return h3 + '<br>' + h2;
+        } else {
+            return h3;
+        }
+    })();
     jews.content = document.getElementById('ndArtBody').innerHTML
                         .split(/<!--.+?기사본문\s*하단.+?-->/)[0].trim()
                         .replace(/(?:style|width|height)=(?:"[^"]+?"|'[^']+?')/g, '').replace(/<p><br><\/p>/g, '');
     jews.timestamp = {
         'created': undefined,
-        'lastModified': new Date(b[0].getElementsByTagName('span')[0].textContent.trim().replace(/\./g, '-').replace(' ', 'T') + '+09:00') // ISO 8601
+        'lastModified': new Date(articleData.childNodes[1].textContent.trim().replace(/\./g, '-').replace(' ', 'T') + '+09:00') // ISO 8601
     };
     jews.reporters = [{
-        'name': j,
-        'mail': b[4].getElementsByTagName('a')[0].textContent.trim().split(' ', 1)[0]
+        'name': articleData.childNodes[2].textContent.trim(),
+        'mail': articleData.querySelectorAll('a[href^="mailto:"]')[0].textContent.trim()
     }];
 };
 parse['뉴데일리경제'] = function (jews) {
