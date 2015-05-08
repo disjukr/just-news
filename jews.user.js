8d1d995dc92b4352197a74cc6da0828687e6c623
JongChan Choi <disjukr@naver.com>
2015-05-08 17:11:53 +0900

    remove all event

--- jews.user.js
+++ jews.user.js
@@ -2194,8 +2194,11 @@ function reconstruct() {
     })();
     // popup 창 못 띄우게 만들기
     window.open = function () {};
+    // 문서에 걸려있는 이벤트 전부 뜯어내기 ...안 뜯어지나?
+    var root = document.createElement('html');
+    document.replaceChild(root, document.documentElement);
     // 페이지 재구축
-    document.body.parentElement.innerHTML = [
+    root.innerHTML = [
         '<head>',
             '<title>', jews.title || 'jews', '</title>',
             '<style>',
