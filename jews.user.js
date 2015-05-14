265590fbd25352292eecbb5a8ec3484a8e175eec
Jaehoon You <teslamint@users.noreply.github.com>
2015-05-14 12:26:21 +0900

    code cleanup

--- jews.user.js
+++ jews.user.js
@@ -722,20 +722,21 @@ parse['뉴시스'] = function (jews) {
 };
 parse['뉴스1'] = function (jews) {
     var news_article = document.querySelector('#articles_detail').cloneNode(true);
+    var news_info = document.querySelector('.info').textContent.trim().split('|');
     jews.title = document.querySelector('h2').textContent.trim();
     jews.subtitle = document.querySelector('.title').textContent.trim().split('\n')[1].trim();
     jews.content = (function () {
         return clearStyles(news_article).innerHTML;
     })();
     jews.timestamp = (function() {
-        var time_info = document.querySelector('.info').textContent.trim().split('|')[1].trim();
+        var time_info = news_info[1].trim();
         return {
             created: new Date(new Date(time_info.replace(/\./g,'-').replace(/[^0-9\-\:\s]/g, '').trim())),
             lastModified: undefined
         };
     })();
     jews.reporters = (function(){
-        var reporters = document.querySelector('.info').textContent.trim().split('|')[0].trim().replace(/\(.+\)/, '').trim();
+        var reporters = news_info[0].trim().replace(/\(.+\)/, '').trim();
         var matches = clearStyles(news_article).innerHTML.match(/[a-z0-9_]+@/);
         return [{
             name: reporters.split(','),
