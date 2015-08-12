5604d23a06e24f9fac30e105667909eb560239c6
Chayoung You <yousbe@gmail.com>
2015-08-12 22:28:07 +0900

    Reparse 서울경제 (Fix #169)

--- jews.user.js
+++ jews.user.js
@@ -1259,7 +1259,48 @@ parse['블로터닷넷'] = function (jews) {
     };
 };
 // ㅅ
-parse['서울경제'] = parse['데일리한국'];
+parse['서울경제'] = function (jews) {
+    jews.title = document.querySelector('#GS_TitleGroup > .newstitle').innerHTML.replace(/<!--\/?DCM_TITLE-->/g, '').trim();
+    jews.subtitle = document.querySelector('#GS_TitleGroup > .subtitle').innerHTML.replace(/<!--\/?DCM_SUBTITLE-->/g, '').trim() || undefined;
+    jews.content = (function () {
+        var content = document.getElementById('GS_Content').cloneNode(true);
+        [].forEach.call(content.querySelectorAll('#frm_AD_GISA_PHOTO_LINE, #AD_GISA_PHOTO_LINE'), function (v) {
+            v.parentNode.removeChild(v);
+        });
+        return clearStyles(content).innerHTML;
+    })();
+    jews.timestamp = (function () {
+        var parsedData = document.getElementById('copyright').textContent;
+        var createdMatch = parsedData.match(/입력시간 : (\d+\/\d+\/\d+\s+\d+:\d+:\d+)/);
+        var lastModifiedMatch = parsedData.match(/수정시간 : (\d+\/\d+\/\d+\s+\d+:\d+:\d+)/);
+        var created;
+        var lastModified;
+        if (createdMatch) {
+            created = new Date(createdMatch[1]);
+        }
+        if (lastModifiedMatch) {
+            lastModified = new Date(lastModifiedMatch[1]);
+        }
+        return {
+            created: created,
+            lastModified: lastModified
+        };
+    })();
+    jews.reporters = (function () {
+        var parsedData = document.querySelector('#GS_TitleGroup > .report');
+        var name = [].slice.call(parsedData.childNodes).filter(function (v) {
+            return v.nodeType === 3;
+        }).map(function (v) {
+            return v.textContent;
+        }).join().trim();
+        var mail = parsedData.querySelector('a').getAttribute('href').replace('mailto:', '');
+
+        return [{
+            name: name,
+            mail: mail
+        }];
+    })();
+};
 parse['서울신문'] = function (jews) {
     jews.title = $('.title_main').contents().eq(0).text().trim();
     jews.subtitle = $('.title_sub').text() || undefined;
