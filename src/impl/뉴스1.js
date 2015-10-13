4c73a1924cb40bf085901673ab60369752e611d8
Kim, Jinsu <item4_hun@hotmail.com>
2015-10-14 02:26:10 +0900

    뉴스1 수리

--- src/impl/뉴스1.js
+++ src/impl/뉴스1.js
@@ -1,31 +1,32 @@
+import $ from 'jquery';
 import { clearStyles } from '../util';
 
 export default function () {
     let jews = {};
-    var news_article = document.createElement('div');
-    news_article.innerHTML = document.getElementById('articles_detail').innerHTML.split('<!-- news1_bottom_468*60 -->')[0].trim();
-    [].slice.call(news_article.getElementsByTagName('iframe')).forEach(function (v) {v.remove()});
+    var headline = $('div.title');
+    var content = $('#articles_detail')[0].cloneNode(true);
 
-    var news_info = document.querySelector('.info').textContent.trim().split('|');
-    jews.title = document.querySelector('h2').textContent.trim();
-    jews.subtitle = document.querySelector('.title').textContent.trim().split('\n')[1].trim();
+    jews.title = $('h2', headline).text();
+    jews.subtitle = $('h3', headline).text().replace(/\n/g, '<br>');
     jews.content = (function () {
-        return clearStyles(news_article).innerHTML;
+        return clearStyles(content).innerHTML;
     })();
     jews.timestamp = (function() {
-        var time_info = news_info[1].trim();
         return {
-            created: new Date(time_info.replace(/\./g,'/').replace(/[^0-9\/\:\s]/g, '').trim()),
+            created: new Date($('div.info', headline).text().split('|')[1].replace(' 송고', '').replace(/\./g, '/').replace(' ', 'T').replace(/\s+/g, '')+'+0900'),
             lastModified: undefined
         };
     })();
     jews.reporters = (function(){
-        var reporters = news_info[0].trim().replace(/\(.+\)/, '').trim();
-        var matches = clearStyles(news_article).innerHTML.match(/[a-z0-9_]+@/);
-        return [{
-            name: reporters.split(','),
-            mail: matches[0] ? matches[0] : undefined
-        }];
+        var reporters = $('div.info', headline).text().split('|')[0].replace(/\(.+?\)/, '').split(', ');
+        var result = [];
+        for (var x in reporters) {
+            result.push({
+                name: reporters[x],
+                mail: undefined
+            });
+        }
+        return result;
     })();
     return jews;
 }
