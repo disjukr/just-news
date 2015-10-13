b482fca9871be03ee0931af2aa2b98eda33f1fe8
Kim, Jinsu <item4_hun@hotmail.com>
2015-10-13 18:58:58 +0900

    조선일보 markup 변경에 따른 재작성

--- src/impl/조선일보.js
+++ src/impl/조선일보.js
@@ -3,48 +3,12 @@ import { clearStyles } from '../util';
 
 export default function () {
     let jews = {};
-    jews.title = $('#title_text').text();
+    jews.title = $('#news_title_text_id').text();
+
     jews.subtitle = undefined;
     jews.content = (function () {
-        var content = $('.article')[0].cloneNode(true);
-        $('.promotion', content).remove();
-        $('div[class*=date_]', content).remove();
-        $('#pop_videobox', content).remove();
-
-        var image_box = $('.center_img_2011', content);
-        image_box.forEach(function (el) {
-            var idx = parseInt($(el).attr('id').match(/\d+/), 10);
-            var player = $('#player' + idx);
-
-            // Image Type 1 (Simple)
-            if ($('dl > dd', player).length === 0) {
-                $(el).replaceWith($('dl > div > img', player)[0].outerHTML);
-                return;
-            }
-
-            // Image Type 2 (Without link)
-            if ($('dl > dd > div > img', player).length !== 0) {
-                $(el).replaceWith($('dl > dd > div > img', player)[0].outerHTML);
-                return;
-            }
-
-            var link = $("dl > dd > div > a", el).attr('onclick');
-
-            // Image Type 3 (With link)
-            if (link === null) {
-                $(el).replaceWith($('dl > dd > div > a > img', player)[0].outerHTML);
-                return;
-            }
-
-            // Should I do this??
-            if (typeof video_tags === 'undefined') {
-                eval($(".article script")[0].text);
-            }
-
-            // Video
-            var video_id = parseInt(link.match(/\d+/), 10);
-            $(el).replaceWith(video_tags[video_id]);
-        });
+        var content = $('#news_body_id')[0].cloneNode(true);
+        $('#date_text, #keyword, .copy_2011', content).remove();
 
         return clearStyles(content).innerHTML;
     })();
@@ -65,9 +29,13 @@ export default function () {
             lastModified: lastModified
         };
     })();
-    jews.reporters = [{
-        name: $('#j1').text().trim().split(' ')[0],
-        mail: $('.j_con_li a').text() || undefined
-    }];
+    jews.reporters = [];
+
+    $('.news_title_author a').each(function(){
+        jews.reporters.push({
+            name: $(this).text(),
+            mail: undefined
+        });
+    });
     return jews;
 }
