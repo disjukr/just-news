bebe00939db3fb3d137594a3754bb492f0de87b2
Kim, Jinsu <item4_hun@hotmail.com>
2015-10-16 15:39:12 +0900

    change name and mail

--- jobs/release.sh
+++ jobs/release.sh
@@ -1,8 +1,8 @@
 set -e
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = false ] && [ "$GITHUB_TOKEN" != "" ]; then
     echo "machine github.com login reactjs-bot password $GITHUB_TOKEN" >~/.netrc
-    git config --global user.name "Travis CI"
-    git config --global user.email "travis@reactjs.org"
+    git config --global user.name "jews-bot"
+    git config --global user.email "jews@chan.moe"
 
     npm run production
 
