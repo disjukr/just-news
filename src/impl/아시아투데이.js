8303a447b99e0558c3f6c934b011948c1c70ebef
JongChan Choi <disjukr@naver.com>
2015-11-06 23:21:26 +0900

    close #194

--- src/impl/아시아투데이.js
+++ src/impl/아시아투데이.js
@@ -3,29 +3,24 @@ import { clearStyles } from '../util';
 
 export default function () {
     let jews = {};
-    jews.title = $('.news_title').text();
-    jews.subtitle = $('.sub_title').text() || undefined;
+    jews.title = $('.article_head dt').text().trim();
     jews.content = (function () {
         var content = $('.news_bm')[0].cloneNode(true);
         $('.phototitle[style="display: none;"], .hidephotocaption, .photocaption[style="display: none;"], .daum_ddn_area, #body_relnews', content).remove();
+        $('iframe[src^="/comm/ads"], iframe[id^=google_osd]', content).remove();
         content.innerHTML = content.innerHTML.split('<!-- //news_body -->')[0];
         return clearStyles(content).innerHTML;
     })();
     jews.timestamp = (function () {
-        var parsedData = $('.news_date').text().split(',');
+        var parsedData = $('.article_head .wr_day').text();
         var timeRegex = /\d{4}-\d{2}-\d{2} \d{2}:\d{2}/;
-        var lastModified;
-        if (parsedData.length > 1) {
-            lastModified = new Date(parsedData[1].match(timeRegex)[0].replace(/-/g, '/'));
-        }
         return {
-            created: new Date(parsedData[0].match(timeRegex)[0].replace(/-/g, '/')),
-            lastModified: lastModified
+            created: new Date(parsedData.match(timeRegex)[0].replace(/-/g, '/'))
         };
     })();
     jews.reporters = [{
-        name: $('.gisa_moree a').contents().eq(0).text().trim(),
-        mail: $('.gija_mail').text()
+        name: $('.writer_info > .fwb > a').text().trim(),
+        mail: $('.article_body > .fl').text().trim()
     }];
     jews.cleanup = function () {
         $('#fb-root, #scrollDiv').remove();
