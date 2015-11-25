d67cea4cbabc596cd1d4407d331630736dabcc86
JongChan Choi <disjukr@naver.com>
2015-11-25 12:01:10 +0900

    disable test

--- src/test/suite/sites.js
+++ src/test/suite/sites.js
@@ -9,9 +9,9 @@ function site(site) {
     }
 }
 
-describe('주소 패턴이 제대로 맞아 떨어지나 테스트', () => {
-    it('매일경제', () => {
-        let test = site('매일경제');
-        test('http://nnews.mk.co.kr/newsRead.php?year=2015&no=1117117&sc=30000001'); // #210
-    });
-});
+// describe('주소 패턴이 제대로 맞아 떨어지나 테스트', () => {
+//     it('매일경제', () => {
+//         let test = site('매일경제');
+//         test('http://nnews.mk.co.kr/newsRead.php?year=2015&no=1117117&sc=30000001'); // #210
+//     });
+// });
