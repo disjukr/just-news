ffaa89bf4604039bc52f037e87395f7ea0f02e49
JongChan Choi <disjukr@naver.com>
2015-11-28 01:01:29 +0900

    more test #208

--- src/test/suite/basic.js
+++ src/test/suite/basic.js
@@ -5,10 +5,14 @@ const { BrowserWindow, ipcMain } = electron;
 
 function jews(url) {
     return new Promise((resolve, reject) => {
+        if (jews.cache[url]) return resolve(jews.cache[url]);
         let window = new BrowserWindow({
             show: process.env.JEWS_SHOW_ELECTRON_WINDOW === 'show'
         });
-        ipcMain.on('jews-done', (event, result) => resolve(result));
+        ipcMain.on('jews-done', (event, result) => {
+            jews.cache[url] = result;
+            resolve(result);
+        });
         ipcMain.on('jews-error', (event, err) => reject(err));
         window.loadURL(url);
         window.webContents.executeJavaScript(`
@@ -20,6 +24,7 @@ function jews(url) {
         `);
     });
 }
+jews.cache = {};
 
 describe('파싱 중에 에러가 나면 안됨', function () {
     this.timeout(0);
@@ -27,6 +32,7 @@ describe('파싱 중에 에러가 나면 안됨', function () {
         await jews('http://www.hani.co.kr/arti/society/schooling/718916.html'); // #211
     });
 });
+
 
 async function testTimestampParsing(url) {
     let jewsResult = await jews(url);
@@ -41,5 +47,21 @@ describe('날짜 파싱에 실패하면 안됨', function () {
     this.timeout(0);
     it('마이데일리', async () => {
         await testTimestampParsing('http://www.mydaily.co.kr/new_yk/html/read.php?newsid=201511192101592220&ext=na'); // #208
+    });
+});
+
+
+async function testTitleParsing(url) {
+    let jewsResult = await jews(url);
+    switch (jewsResult.title) {
+    case undefined: throw new Error('제목이 undefined');
+    case '': throw new Error('제목이 비어있음');
+    }
+}
+
+describe('제목이 비어있으면 안됨', function () {
+    this.timeout(0);
+    it('마이데일리', async () => {
+        await testTitleParsing('http://www.mydaily.co.kr/new_yk/html/read.php?newsid=201511192101592220&ext=na'); // #208
     });
 });
