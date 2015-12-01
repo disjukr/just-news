c9ff078140eace55f2ee3a9d82633bca88a8d419
Chayoung You <yousbe@gmail.com>
2015-12-01 14:18:39 +0900

    Add sites test for 스포츠서울

--- src/test/suite/sites.js
+++ src/test/suite/sites.js
@@ -14,4 +14,9 @@ describe('주소 패턴이 제대로 맞아 떨어지나 테스트', () => {
         let test = site('매일경제');
         test('http://nnews.mk.co.kr/newsRead.php?year=2015&no=1117117&sc=30000001'); // #210
     });
+
+    it('스포츠서울', () => {
+        let test = site('스포츠서울');
+        test('http://www.sportsseoul.com/news/read/326593'); // #216
+    });
 });
