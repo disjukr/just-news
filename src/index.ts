709579298188acb6b4d49d5fcbd69e1767d83a34
OnLiU <jhs_onliu@naver.com>
2017-10-26 22:05:55 +0900

    optimise index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,50 +5,47 @@ import {
     reconstructable,
 } from './reconstruct';
 
+export function checkUrl(pattern: string): boolean {
+    const { href } = window.location;
 
-}
-
-}
-
-export function waitWhilePageIsLoading() {
-    return new Promise(resolve => {
-        switch (document.readyState) {
-        case 'interactive': case 'complete': { resolve(); break; }
-        default: {
-            window.addEventListener('DOMContentLoaded', resolve);
-        } break;
-        }
-    });
-};
-
-export function checkUrl(pattern: string, url=window.location.href) {
     return (new RegExp(
         escapeRegExp(pattern).replace(/\\\*/g, '.*')
-    )).test(url);
-};
+    )).test(href);
+}
 
-export function here() {
-    for (let site in sites) {
-        for (let pattern of sites[site]) {
+export function where(): string | void {
+    for (const site in sites) {
+        for (const pattern of sites[site]) {
             if (checkUrl(pattern)) {
                 return site;
             }
         }
     }
+
     throw new Error('이 사이트는 지원되지 않습니다.');
-};
+}
+
+export function waitWhilePageIsLoading(): Promise<{}> {
+    return new Promise(resolve => {
+        switch (document.readyState) {
+        case 'interactive':
+        case 'complete':
+            resolve();
+            break;
+        default:
+            window.addEventListener('DOMContentLoaded', resolve);
+            break;
+        }
+    });
+}
 
 main: {
     if (!reconstructable()) {
         break main;
     }
-    waitWhilePageIsLoading().then(() => {
-        const where = here();
-        return require('./impl/' + where).default();
-    }).then(
-        article => reconstruct(article)
-    ).catch(e => {
-        let err = e ? (e.stack || e) : e;
-        console.error(err);
-    });
+
+    waitWhilePageIsLoading()
+    .then(() => require('./impl/' + where()).default())
+    .then(article => reconstruct(article))
+    .catch(err => console.log(err && (err.stack || err)));
 }
