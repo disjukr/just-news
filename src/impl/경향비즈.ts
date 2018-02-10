f7b318e5d8969d727164141fd50360e8e139dd98
JongChan Choi <jong@chan.moe>
2018-02-11 00:22:59 +0900

    경향비즈 타입스크립트

--- src/impl/경향비즈.ts
+++ src/impl/경향비즈.ts
@@ -1,9 +1,13 @@
 import * as $ from 'jquery';
 import * as moment from 'moment';
+
+import {
+    Article,
+} from '..';
 import { clearStyles } from '../util';
 
 
-export function parse() {
+export function parse(): Article {
     const articleBodyElement = $('.art_cont')[0].cloneNode(true);
     return {
         title: $('#articleTtitle').text(),
@@ -24,7 +28,7 @@ export function parse() {
         timestamp: (() => {
             const times = $('#bylineArea em');
             const format = 'YYYY.MM.DD HH:mm:ss';
-            const parse = text => moment(text.replace(/.*?:\s*/, ''), format);
+            const parse = (text: string) => moment(text.replace(/.*?:\s*/, ''), format);
             return {
                 created: parse(times.eq(0).text()).toDate(),
                 lastModified: times[1] && parse(times.eq(1).text()).toDate(),
@@ -33,9 +37,9 @@ export function parse() {
         reporters: (() => {
             const d = /(.*?) 기자 ?(.*)/.exec($('.view_header .subject .name').text());
             return [{
-                name: d[1],
-                mail: d[2],
-            }]
+                name: d![1],
+                mail: d![2],
+            }];
         })(),
         cleanup: () => $('#scrollDiv, iframe').remove(),
     };
