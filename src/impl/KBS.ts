08ad306261aa7894f517ecbe025bb1498c4d54e7
KBuild <qwer7995@gmail.com>
2019-02-18 13:35:56 +0900

    refactor

--- src/impl/KBS.ts
+++ src/impl/KBS.ts
@@ -7,17 +7,12 @@ export const readyToParse = () => waitElement('.name span');
 export function parse(): Article {
     return {
         title: $('.landing-caption .tit-s').text(),
-        content: (() => {
-          const ret =
+        content:
             $('script').filter((_, scriptTag) => {
-              if(scriptTag.innerHTML.includes('displayVod')) { return scriptTag }
-            }).map((_, element) => {
-              return element.outerHTML
-            }).get().join(' ') +
-            $('.detail-visual')[0].outerHTML + '<br>' +
-            clearStyles($('#cont_newstext')[0].cloneNode(true) as HTMLElement).innerHTML
-          return ret;
-        })(),
+                if(scriptTag.innerHTML.includes('displayVod')) { return scriptTag }
+            }).map((_, element) => element.outerHTML).get().join(' ') + //video runtime script
+            $('.detail-visual')[0].outerHTML + '<br>' + //video control elements
+            clearStyles($('#cont_newstext')[0].cloneNode(true) as HTMLElement).innerHTML,
         reporters :[{
             name: $('.name span:eq(0)').text(),
             mail: $('.name span:eq(1)').text()
