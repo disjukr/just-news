572fc4c3d4a4e1e2e2781a9ec03b33381d76c1b9
Chayoung You <yousbe@gmail.com>
2019-02-18 20:59:35 +0900

    Fix parsing bugs for nocutnews.co.kr (Fix #308)

--- src/impl/노컷뉴스.ts
+++ src/impl/노컷뉴스.ts
@@ -1,27 +1,34 @@
 import * as $ from 'jquery';
-import { clearStyles } from '../util';
+import {
+    clearStyles,
+    waitElement
+} from '../util';
 import { Article } from 'index';
 
+export const readyToParse = () => waitElement('.view_foot');
+
 export const cleanup = () => {
-    $('#scrollDiv').remove();
+    $('#scrollDiv, body>img').remove();
 }
 
 export function parse(): Article {
     return {
-        title: $('.reporter_info h2').text(),
-        subtitle: $('.viewbox h3').text(),
+        title: $('.h_info h2').text(),
+        subtitle: $('.viewbox h3').html(),
         content: (() => {
             const content = $('#pnlContent')[0].cloneNode(true);
+            $('.viewpic>iframe', content).remove();
+            $('div[style="text-align:right;width:250px;float:right;margin:15px 0px 0px 15px;"]', content).remove();
             $('.relatednews', content).remove();
             return clearStyles(content).innerHTML;
         })(),
         timestamp: {
-            created: new Date($('.reporter_info ul li:first-child span').text().replace(/-/g, '/')),
+            created: new Date($('.h_info ul li:nth-child(2) span').text().replace(/-/g, '/')),
             lastModified: undefined
         },
         reporters: [{
-            name: $('.reporter_info .email span').text(),
-            mail: $('.reporter_info .email a').attr('title')
+            name: $('.h_info .email span').text(),
+            mail: $('.h_info .email a').attr('title')
         }],
     };
 }
