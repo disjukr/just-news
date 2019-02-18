f73430a1a790a8fd3d9cfd971116121a153b4d1b
KBuild <qwer7995@gmail.com>
2019-02-18 12:25:44 +0900

    make content with kbs video wrapper(JW_PLAYER)

--- src/impl/KBS.ts
+++ src/impl/KBS.ts
@@ -7,7 +7,17 @@ export const readyToParse = () => waitElement('.name span');
 export function parse(): Article {
     return {
         title: $('.landing-caption .tit-s').text(),
-        content: clearStyles($('#cont_newstext')[0].cloneNode(true) as HTMLElement).innerHTML,
+        content: (() => {
+          const ret =
+            $("script").filter((_, scriptTag) => {
+              if(scriptTag.innerHTML.includes("displayVod")) { return scriptTag }
+            }).map((_, element) => {
+              return element.outerHTML
+            }).get().join(' ') +
+            $(".detail-visual")[0].outerHTML + '<br>' +
+            clearStyles($('#cont_newstext')[0].cloneNode(true) as HTMLElement).innerHTML
+          return ret;
+        })(),
         reporters :[{
             name: $('.name span:eq(0)').text(),
             mail: $('.name span:eq(1)').text()
