5d02edf52cf38040ecf8706f08c66dbadd4d7107
Sunguk Lee <d3m3vilurr@gmail.com>
2019-02-20 22:54:28 +0900

    MBC: Fix parsing problem.
    this patch have these changes:
    - support last modified time
    - support movie player

--- src/impl/MBC.ts
+++ src/impl/MBC.ts
@@ -2,16 +2,26 @@ import * as $ from 'jquery';
 import { clearStyles } from '../util';
 import { Article } from 'index';
 
+const toDate = (elem) => new Date(elem.text().trim().split(/\s/).slice(-2).join(' '));
+
 export function parse(): Article {
+    // reporter | created | lastModified
+    const timeSection = $('#news_content_1 .wrap_time span');
+
     return {
-        title: $('#content .view-title').text(),
-        content: clearStyles($('#DivPrint .view-con')[0].cloneNode(true)).innerHTML,
+        title: $('#news_content_1 .title').text(),
+        content: (() => {
+            const content = $('#news_content_1 .cont .body')[0].cloneNode(true);
+            $('div.print_video_img', content).remove();
+
+            return clearStyles(content).innerHTML;
+        })(),
         timestamp: {
-            created: new Date($('#DivPrint .article-time-date').text().replace(/-/g, '/')),
-            lastModified: undefined
+            created: toDate($(timeSection.get(1))),
+            lastModified: timeSection.length === 3 ? toDate($(timeSection.get(2))) : undefined
         },
         reporters: [{
-            name: $('#DivPrint .reporter').text().trim().split(/\s+/)[0],
+            name: $($('#news_content_1 .wrap_time span').get(0)).text().trim(),
             mail: undefined
         }]
     };
