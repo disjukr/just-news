ce2b8d088ff4bfbbbe3741d8844051515a7a5d15
JongChan Choi <jong@chan.moe>
2019-05-07 13:13:57 +0900

    health check wip3

--- src/test/health-check/index.ts
+++ src/test/health-check/index.ts
@@ -40,6 +40,7 @@ const cases: Case[] = [
 
 async function run() {
     const jobs = [...cases];
+    const jobResults: JobResult[] = [];
     //*
     const headless = false;
     const workers = [0, 1];
@@ -47,31 +48,80 @@ async function run() {
     const headless = true;
     const workers = [0, 1, 2, 3, 4, 5, 6, 7];
     //*/
+    type JobResult = (JobError | JobOk) & { duration: number };
+    interface JobError {
+        type: 'error';
+        error: Error;
+    }
+    interface JobOk extends UnwrapPromise<ReturnType<typeof doJob>> {
+        type: 'ok';
+    }
     const browser = await puppeteer.launch({ headless });
     await Promise.all(workers.map(async () => {
         let job: Case;
         while (job = jobs.pop()!) {
-            const impl = getImpl(job.impl);
             const page = await browser.newPage();
-            const waitForSelector = async (selector: string) => void await Promise.race([
-                page.waitForSelector(selector, { timeout: 0 } ),
-                wait(3000),
-            ]);
-            await Promise.race([
-                page.goto(job.url, { timeout: 0 }),
-                ...(impl.readyToParse ? [impl.readyToParse(waitForSelector)] : [] as any),
-                wait(3000),
-            ]);
-            const article = fromJSON(await page.evaluate(`
-                new Promise(async resolve => {
-                    const article = ${ fs.readFileSync('./tmp/health-check.js', 'utf8') };
-                    resolve(await article.default);
-                })
-            `));
-            console.log(job.impl, article);
+            const startTime = Date.now();
+            const jobResult = await doJob(job, page).then(
+                result => ({ type: 'ok' as const, ...result }),
+            ).catch(
+                error => ({ type: 'error' as const, error }),
+            );
+            const duration = Date.now() - startTime;
+            jobResults.push({ ...jobResult, duration });
             await page.close();
         }
     }));
     await browser.close();
+    console.log(jobResults);
 }
 run();
+
+async function doJob(job: Case, page: puppeteer.Page) {
+    const impl = getImpl(job.impl);
+    const waitForSelector = async (selector: string) => void await Promise.race([
+        page.waitForSelector(selector, { timeout: 0 } ),
+        wait(3000),
+    ]);
+    await Promise.race([
+        page.goto(job.url, { timeout: 0 }),
+        ...(impl.readyToParse ? [impl.readyToParse(waitForSelector)] : [] as any),
+        wait(3000),
+    ]);
+    const article = fromJSON(await page.evaluate(`
+        new Promise(async resolve => {
+            const article = ${ fs.readFileSync('./tmp/health-check.js', 'utf8') };
+            resolve(await article.default);
+        })
+    `));
+    type ProblemReason = 'missing' | 'invalid';
+    type Problem = [string/* path */, ProblemReason];
+    interface JobOkResult extends Case {
+        article: typeof article;
+        problems: Problem[];
+    }
+    if (!article) return { ...job, article, problems: [] } as JobOkResult;
+    return {
+        ...job,
+        article,
+        problems: job.check.map(path => {
+            const value = pincet(article, path);
+            if (!value) return [path, 'missing'] as const;
+            if (path.startsWith('timestamp.')) {
+                if (isNaN(+value)) return [path, 'invalid'] as const;
+            }
+            return null;
+        }).filter(x => x),
+    } as JobOkResult;
+}
+
+function pincet(obj: any, path: string): any {
+    let result = obj;
+    for (const fieldName of path.split('.')) {
+        if (!result) return undefined;
+        if (typeof result !== 'object') return undefined;
+        if (!(fieldName in result)) return undefined;
+        result = result[fieldName];
+    }
+    return result;
+}
--- types/sugar.d.ts
+++ types/sugar.d.ts
@@ -1,1 +1,2 @@
 type Nullable<T> = T | null | undefined;
+type UnwrapPromise<T> = T extends Promise<infer U> ? U : never;
