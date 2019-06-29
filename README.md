55ca2013b11f080d200a777ebfa876d0473e2637
유정민 <machenity@gmail.com>
2019-06-29 15:33:21 +0900

    주간경향 추가

--- README.md
+++ README.md
@@ -182,6 +182,7 @@ export const readyToParse = () => waitElement('#comments');
 * [x] [전자신문](http://www.etnews.com)
 * [x] [조선비즈](http://biz.chosun.com)
 * [x] [조선일보](http://www.chosun.com)
+* [x] [주간경향](http://weekly.khan.co.kr)
 * [x] [중앙데일리](http://koreajoongangdaily.joins.com)
 * [x] [중앙일보](http://joongang.joins.com)
 * [x] [지디넷코리아](http://www.zdnet.co.kr)
---
+++ src/impl/주간경향.ts
@@ -0,0 +1,46 @@
+import * as $ from 'jquery';
+
+import {
+    Article,
+    ReadyToParse,
+} from '..';
+import {
+    clearStyles,
+} from '../util';
+
+
+export const readyToParse: ReadyToParse = wait => wait('#articleBody');
+
+export const cleanup = () => $('#scrollDiv, iframe').remove();
+
+export function parse(): Article {
+    const title = $('.title')[0].cloneNode(true);
+    const reporter = $('.byline_wrap', title);
+    reporter.remove();
+    const reporterText = reporter.text();
+    return {
+        title: (() => {
+            return $(title).text().trim();
+        })(),
+        content: (() => {
+            const articleBodyElement = $('.article_txt')[0].cloneNode(true) as HTMLElement;
+            return clearStyles(articleBodyElement).innerHTML;
+        })(),
+        timestamp: (() => {
+            const times = $('.article_date');
+            return {
+                created: new Date(times[0].childNodes[0].textContent!.trim().replace(/\./g, "-")),
+            };
+        })(),
+        reporters: (() => {
+            if (!reporterText) {
+                return [];
+            }
+            const seperator = reporterText.lastIndexOf(" ");
+            return [{
+                name: reporterText.substr(0, seperator),
+                mail: reporterText.substr(seperator),
+            }];
+        })(),
+    };
+}
