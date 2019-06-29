ee48a88f727ec89fe36954bcef1a15c121eb41ac
Park Hyunwoo <ez.amiryo@gmail.com>
2019-06-29 16:36:53 +0900

    한국경제 파서 수정

--- src/impl/한국경제.ts
+++ src/impl/한국경제.ts
@@ -6,24 +6,24 @@ export const cleanup = () => {
     $('#scrollDiv, iframe').remove();
 }
 
+export const readyToParse: ReadyToParse = wait => wait('.article_btm_box');
+
 export function parse(): Article {
     return {
-        title: $('.news-atc-tit .atc-tit').text(),
+        title: $('h1.title').text(),
         subtitle: $('.article_stit').text().trim(),
         content: (() => {
             const content = $($('#newsView')[0].cloneNode(true));
-            $('.article_stit, .article_aside_group, .ico_imgMore', content).remove();
+            $('.article_stit, .article_aside_group, .ico_imgMore, .box_related_list', content).remove();
             return clearStyles(content[0]).innerHTML;
         })(),
         timestamp: (() => {
-            const parsedData = $('.atc-info').children();
-            //?
-            function parseDate(s: any) {
-                return new Date(s.replace(/-/g, '/').replace(/[가-힣]/g, '').replace(/^\s+/g, ''));
-            }
+            let created: any = $('.date-published .num').text().trim().replace(/\./g, "-").replace(/\s+/, "T")
+            let lastModified: any = $('.date-modify .num').text().trim().replace(/\./g, "-").replace(/\s+/, "T")
+
             return {
-                created: parseDate(parsedData.eq(0).text()),
-                lastModified: parseDate(parsedData.eq(1).text())
+                created: new Date(created),
+                lastModified: new Date(lastModified)
             };
         })(),
         reporters: []
