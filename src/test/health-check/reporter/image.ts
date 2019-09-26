5e082dc64fc6899dacb831cc1d11a92d33fab3ef
Kim Jin Su <item4_hun@hotmail.com>
2019-09-26 14:32:47 +0900

    Change Screenshot codes

--- src/test/health-check/reporter/image.ts
+++ src/test/health-check/reporter/image.ts
@@ -5,21 +5,10 @@ const imageReporter: Reporter<void> = async (jobResults, browser) => {
     const htmlReport = await htmlReporter(jobResults, browser);
     const page = await browser.newPage();
     await page.setContent(htmlReport);
-    const rect = await page.evaluate(() => {
-        const element = document.querySelector('.markdown-body');
-        if (!element)
-            throw new Error("Markdown body is not found!")
-        const {left, top, width, height} = element.getBoundingClientRect();
-        return {left, top, width, height, id: element.id};
-    });
-    await page.screenshot({
+    await page.waitFor('main');
+    const mainTag = await page.$('main') || page;
+    await mainTag.screenshot({
         path: './tmp/health-check.png',
-        clip: {
-            x: rect.left,
-            y: rect.top,
-            width: rect.width,
-            height: rect.height,
-        }
     });
 }
 
